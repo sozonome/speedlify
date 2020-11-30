759269d6510f98996b7b9fd1bd20115aa4a5f6c8
Agustinus Nathaniel <berserk.nate@gmail.com>
2020-11-30 14:56:59 +0700

    set freq to hourly test

--- _data/sites/sozonome.js
+++ _data/sites/sozonome.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'sznm.dev', // optional, falls back to object key
   description: "sznm.dev",
   options: {
-    frequency: 60 * 23, // (in minutes), 23 hours
+    frequency: 60, // (in minutes), 23 hours
     freshChrome: 'site',
   },
   urls: [
