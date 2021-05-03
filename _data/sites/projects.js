ff8e9fe13c17c152170c3e83ecedcc47146082de
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-05-03 22:54:13 +0700

    update freq

--- _data/sites/projects.js
+++ _data/sites/projects.js
@@ -5,7 +5,7 @@ module.exports = {
   // // skip if this is a new fork of the speedlify (not Zach’s)
   // skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
   options: {
-    frequency: 60 * 5, // every 6 hours
+    frequency: 30, // max once every 30 minutes
     // Use "run" if the sites don’t share assets on the same origin
     //           and we can reset chrome with each run instead of
     //           each site in every run (it’s faster)
